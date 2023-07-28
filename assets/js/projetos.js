c01448ff2cde9bb482125cca16c3376bdccf2786
estevaonascimento15 <79883013+estevaonascimento15@users.noreply.github.com>
2023-07-27 22:27:51 -0300

    large file track

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -68,7 +68,7 @@ function projetos(section) {
                   />
                 </svg>
               </a>
-              <a href="barbearia">
+              <a href="https://barbearia-nmnc.vercel.app/">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
